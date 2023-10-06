92c9dafdc16aa90348d9467cd10f58c8812dde1a
Edward-789 <142745633+Edward-789@users.noreply.github.com>
2023-10-06 21:07:32 +0100

    Add files via upload

---
+++ TicTacToe/src/maingame.js
@@ -0,0 +1,91 @@
+const notOccupied = -1;
+const xOccupied = 0;
+const oOccupied = 1
+const startSquares = [
+    notOccupied, notOccupied, notOccupied, notOccupied, notOccupied, notOccupied, notOccupied, notOccupied, notOccupied
+]
+const playerMove = document.querySelector(".playerMove")
+
+let board = {
+    occupiedSquares : startSquares,
+    XToMove : true,
+    plyCount : 0,
+    stack : [-1,-1,-1,-1,-1,-1,-1,-1],
+    makeMove : function(move, inThink = true) {
+        this.occupiedSquares[move] = this.XToMove ? xOccupied : oOccupied
+        this.stack[this.plyCount] = move;
+        if (!inThink) {
+            document.getElementById(move.toString()).innerText = this.XToMove ? 'X' : 'O'
+            document.getElementById(move.toString()).style.color = this.XToMove ? 'red' : 'blue';
+        }
+        
+        this.XToMove = !this.XToMove
+        this.plyCount++;
+    },
+    undoMove : function(move) {
+        this.plyCount--;
+        if (this.stack[this.plyCount] != move) 
+            throw new Error("move was never played")
+        this.occupiedSquares[move] = notOccupied
+        this.XToMove = !this.XToMove
+    },
+    resetBoard : function() {
+        this.occupiedSquares = startSquares;
+    },
+    generateMoves : function() {
+        var legalMoves = [];
+
+        if (this.checkWinning() != -1) return legalMoves;
+
+        for(let i = 0; i < 9; i++) {
+            let testMove = this.occupiedSquares[i]
+            if (testMove == notOccupied) 
+                legalMoves.push(i)
+            
+        }
+
+        return legalMoves;
+    },
+    // return value of -1 means nobody is winning
+    // value of 0 means x is winning,
+    // value of 1 means o is winning.
+    checkWinning : function() {
+        // check columns
+        for (let i = 0; i <= 2; i++) {
+            if (this.occupiedSquares[i] == this.occupiedSquares[i + 3] && this.occupiedSquares[i + 3] == this.occupiedSquares[i + 6])
+                return this.occupiedSquares[i]
+        }
+        // check rows
+        for (let i = 0; i <= 6; i += 3) {
+            if (this.occupiedSquares[i] == this.occupiedSquares[i + 1] && this.occupiedSquares[i + 1] == this.occupiedSquares[i + 2])
+                return this.occupiedSquares[i]
+        }
+        // check diagonals
+        if (this.occupiedSquares[0] == this.occupiedSquares[4] && this.occupiedSquares[4] == this.occupiedSquares[8] 
+        || this.occupiedSquares[2] == this.occupiedSquares [4] && this.occupiedSquares[4] == this.occupiedSquares [6])
+        return this.occupiedSquares[4]
+
+
+        return -1;
+    }
+}
+
+document.querySelector("#submitMove").addEventListener("click", function() {
+
+    const move = parseInt(playerMove.value);
+
+    if (isNaN(move) || move < 0 || move > 8) {
+        alert("enter a valid move (0-8).");
+        return;
+    }
+
+    if (!board.generateMoves().includes(move)) {
+        alert("not a legal move buddy.");
+        return;
+    }
+
+    bestMoveRoot = -1;
+    board.makeMove(move, false);
+    search(maxDepth);
+    board.makeMove(bestMoveRoot, false);
+})
---
+++ TicTacToe/src/negamax.js
@@ -0,0 +1,32 @@
+let bestMoveRoot = -1;
+let maxDepth = 8;
+let nodes = 0
+
+function search(depth, alpha, beta) {
+    console.log(nodes++)
+    let legalMoves = board.generateMoves();
+    if (legalMoves.length == 0 || depth == 0) return board.checkWinning() == -1 ? 0 : -99999;
+    let eval;
+    let bestEval = -99999;
+ 
+    for (let i = 0; i < legalMoves.length; i++) {
+        let move = legalMoves[i]; // Get the move from the legalMoves array
+
+        board.makeMove(move);
+        eval = -search(depth - 1, -beta, -alpha);
+        board.undoMove(move);
+
+        if (eval > bestEval) {
+            bestEval = eval;
+            if (depth === maxDepth) {
+                bestMoveRoot = move;
+            }
+            if (eval > alpha) {
+                alpha = eval;
+                if (alpha >= beta) break;
+            }
+        }
+    }
+
+    return bestEval;
+}
