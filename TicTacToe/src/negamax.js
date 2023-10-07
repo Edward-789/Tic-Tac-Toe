fd94d270bc003d0968a023fb09ac2d24c42d9d34
Edward-789 <142745633+Edward-789@users.noreply.github.com>
2023-10-07 08:42:10 +0100

    add -depth to leaf nodes

--- TicTacToe/src/negamax.js
+++ TicTacToe/src/negamax.js
@@ -4,7 +4,7 @@ let nodes = 0
 
 function search(depth, alpha, beta) {
     let legalMoves = board.generateMoves();
-    if (legalMoves.length == 0 || depth == 0) return board.checkWinning() == -1 ? Math.random() : -99999;
+    if (legalMoves.length == 0 || depth == 0) return board.checkWinning() == -1 ? Math.random() : -99999 - depth;
     let eval;
     let bestEval = -99999;
  
