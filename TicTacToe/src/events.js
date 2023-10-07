906d436d8f3e36ab8358823bbe7979999121e103
Edward-789 <142745633+Edward-789@users.noreply.github.com>
2023-10-07 08:26:46 +0100

    make stuff look better

--- TicTacToe/src/events.js
+++ TicTacToe/src/events.js
@@ -1,16 +1,27 @@
-document.querySelector("#submitMove").addEventListener("submit", function() {
+const diaglogText = document.getElementById("dialogText")
+const dialog = document.getElementById("dialog")
 
+
+for (let i = 0; i < 9; i++) {
+    document.getElementById(i).addEventListener("click", function() {
+        playerMove.value = i;
+        document.querySelector("#submitMoveButton").click()
+    }) 
+}   
+
+
+document.querySelector("#submitMove").addEventListener("submit", function() {
     event.preventDefault();
    
     const move = parseInt(playerMove.value);
 
     if (isNaN(move) || move < 0 || move > 8) {
-        alert("enter a valid move (0-8).");
+        showModal("enter a valid move (0-8).");
         return;
     }
 
     if (!board.generateMoves().includes(move)) {
-        alert("not a legal move buddy");
+        showModal("not a legal move buddy");
         return;
     }
 
@@ -34,15 +45,23 @@ document.querySelector("#submitMove").addEventListener("submit", function() {
 
 function alertWhoIsWinning() {
     if (board.checkWinning() != -1) {
-        alert((board.checkWinning() == 0 ? 'X ' : 'O ' ) + "has won.")
-        board.resetBoard()
+        showModal((board.checkWinning() == 0 ? 'X ' : 'O ' ) + "has won.")
         return true;
     } 
     if (board.generateMoves().length == 0) {
-        alert("Draw.")
-        board.resetBoard()
+        showModal("draw")
         return true;
     }
 
     return false;
+}
+
+document.getElementById("dialogButton").addEventListener("click", function() {
+    dialog.close()
+    if(board.checkWinning() != -1 || board.generateMoves().length == 0)board.resetBoard()
+})
+
+function showModal(textDisplayed) {
+    diaglogText.innerText = textDisplayed
+    dialog.showModal()
 }
